1779bdfc3fac95ae3a50f2706391af53332af4c1
sbprabhakar <sbprabhakar@gmail.com>
2026-08-11 08:01:51 +0000

    update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
 name = input("What is your name? ")
 print(f"Hello, {name}!")
+
+age = input("What is your age? ")
+print(f"Hello, {age}!")
+
+city = input("What is your city name? ")
+print(f"Hello, {city}!")
+
+language = input("What is your favorite language? ")
+print(f"Hello, {language}!")
